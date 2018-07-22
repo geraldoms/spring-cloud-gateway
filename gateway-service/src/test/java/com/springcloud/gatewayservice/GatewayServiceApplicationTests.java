4638fcb2be427d4708371f7d9e6a0b8ef5edce39
Geraldo Silva <gmagela@kenzan.com>
2018-07-22 15:53:35 -0400

    Add the gateway-service module for a basic gateway example

---
+++ gateway-service/src/test/java/com/springcloud/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springcloud.gatewayservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class GatewayServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
