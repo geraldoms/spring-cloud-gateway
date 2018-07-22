ddba4c72756beb838ad389556c647530da2eec3e
Geraldo Silva <gmagela@kenzan.com>
2018-07-22 15:19:45 -0400

    First commit - The discovery-server module for a gateway basic example

---
+++ discovery-server/src/test/java/com/springcloud/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springcloud.discoveryserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class DiscoveryServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
